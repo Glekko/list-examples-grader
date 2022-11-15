af2521e233bf64497280853e2a0cea9a681b62ac
Glekko <kevinhuynhwong1492@gmail.com>
2022-11-15 14:45:27 -0800

    added script

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,57 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter(){
+      
+      ListExamples myList = new ListExamples();
+
+      List<String>stringToCheck = new ArrayList<>();
+      stringToCheck.add("b");
+      
+      List<String>checkExpected = new ArrayList<>();
+      checkExpected.add("banana");
+      checkExpected.add("bandana");
+      checkExpected.add("bandaid");
+
+      List<String>input1 = new ArrayList<>();
+      input1.add("banana");  
+      input1.add("bandana");
+      input1.add("bandaid");
+      input1.add("crash");
+
+      assertEquals(checkExpected, ListExamples.filter(input1, stringToCheck));
+
+  }
+
+  @Test
+  public void testMerge(){
+      List<String>firstArray = new ArrayList<>();
+      firstArray.add("apple");
+      firstArray.add("banana");
+      firstArray.add("crab");
+      firstArray.add("error");
+
+      List<String>secondArray = new ArrayList<>();
+      secondArray.add("atlanta");
+      secondArray.add("ball");
+      secondArray.add("crab");
+      secondArray.add("dream");
+
+      List<String>finalArray = new ArrayList<>();
+      finalArray.add("apple");
+      finalArray.add("atlanta");
+      finalArray.add("ball");
+      finalArray.add("banana");
+      finalArray.add("crab");
+      finalArray.add("crab");
+      finalArray.add("dream");
+      finalArray.add("error");
+
+      assertEquals(finalArray, ListExamples.merge(firstArray, secondArray));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,23 @@
 # Create your grading script here
-
-set -e
-
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
-
+cp TestListExamples.java student-submission
+cd student-submission
+if [ -e "ListExamples.java" ]
+then
+    echo "Correct file! Well done!"
+else
+    echo "Wrong file submission!"
+    exit
+fi
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]
+then
+    echo "File did not compile!"
+    exit
+else
+    echo "File successfuly compiled!"
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples &> testOutput.txt
+cat testOutput.txt
